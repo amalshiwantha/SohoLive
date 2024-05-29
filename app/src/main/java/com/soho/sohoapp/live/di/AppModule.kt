3cfa17b1970d6b81626cd9e0d75641bfb18bee66
Amal Ranasinghe <amal@soho.com.au>
2024-05-29 08:59:19 +0530

    361 - network classes connected with DI

--- app/src/main/java/com/soho/sohoapp/live/di/AppModule.kt
+++ app/src/main/java/com/soho/sohoapp/live/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.soho.sohoapp.live.di
 
 import com.soho.sohoapp.live.datastore.AppDataStoreManager
+import com.soho.sohoapp.live.network.api.soho.SohoApiRepository
+import com.soho.sohoapp.live.network.api.soho.SohoApiServices
+import com.soho.sohoapp.live.network.api.soho.SohoServicesImpl
 import com.soho.sohoapp.live.network.core.KtorHttpClient
 import com.soho.sohoapp.live.view.screens.splash.SplashViewModel
 import kotlinx.serialization.json.Json
@@ -9,8 +12,17 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+    // Serialization
     single { Json { isLenient = true; ignoreUnknownKeys = true } }
+    // Network
     single { KtorHttpClient.httpClient() }
+    //DataStore
     single { AppDataStoreManager(androidContext()) }
+    //Services
+    single<SohoApiServices> { SohoServicesImpl(get()) }
+    //Repositories
+    single { SohoApiRepository(get()) }
+
+    //ViewModels
     viewModel { SplashViewModel(get()) }
 }
