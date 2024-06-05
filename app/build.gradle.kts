c84704712675e1c9ab53613de99492a76dbaba85
Amal Ranasinghe <amal@soho.com.au>
2024-06-05 13:17:08 +0530

    361 -  updated navigation-compose lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation(libs.ktor.negotiation)
 
     //bottom nav
-    implementation("androidx.navigation:navigation-compose:2.7.0-rc01")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
