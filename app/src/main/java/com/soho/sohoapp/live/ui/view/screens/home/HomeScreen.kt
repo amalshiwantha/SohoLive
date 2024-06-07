f3c8d3989f5bfe4d4771e20c59bb412b27077835
Amal Ranasinghe <amal@soho.com.au>
2024-06-07 09:11:28 +0530

    371 - dynamically add topAction bar title

--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/home/HomeScreen.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/home/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -31,7 +32,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -44,6 +44,7 @@ import com.soho.sohoapp.live.ui.components.AppTopBar
 import com.soho.sohoapp.live.ui.components.BottomNavigationBar
 import com.soho.sohoapp.live.ui.components.brushMainGradientBg
 import com.soho.sohoapp.live.ui.navigation.BottomNavHost
+import com.soho.sohoapp.live.ui.navigation.NavigationPath
 import com.soho.sohoapp.live.ui.view.activity.HaishinActivity
 
 @Composable
@@ -51,16 +52,29 @@ fun HomeScreen(navControllerHome: NavHostController, homeVm: HomeViewModel = vie
 
     val context = LocalContext.current
     val uiState by homeVm.uiState.collectAsState()
-    var navigationSelectedItem by remember { mutableIntStateOf(0) }
     val navController = rememberNavController()
+    var navigationSelectedItem by remember { mutableIntStateOf(0) }
+    var selectedTabTitle by remember { mutableStateOf("SCHEDULED") }
+
+    LaunchedEffect(navController) {
+        navController.currentBackStackEntryFlow.collect { backStackEntry ->
+            selectedTabTitle = when (backStackEntry.destination.route) {
+                NavigationPath.SCHEDULED.name -> context.getString(R.string.scheduled_livestreams)
+                NavigationPath.GO_LIVE.name -> context.getString(R.string.create_livestream)
+                NavigationPath.VIDEO_LIBRARY.name -> context.getString(R.string.your_videos)
+                NavigationPath.PROFILE.name -> context.getString(R.string.manage_profile)
+                else -> context.getString(R.string.scheduled_livestreams)
+            }
+        }
+    }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
             AppTopBar(
-                title = stringResource(R.string.signin_title),
+                title = selectedTabTitle,
                 isAllowBack = false,
-                onBackClick = { navController.popBackStack() })
+                onBackClick = { })
         },
         bottomBar = {
             BottomNavigationBar(navController, navigationSelectedItem, onTabClick = {
