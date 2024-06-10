46ff3e8db256431f3d74f0b96dc50227654cb998
Amal Ranasinghe <amal@soho.com.au>
2024-06-10 11:19:33 +0530

    371 - designed bottom next button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,8 @@ dependencies {
     //bottom nav
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/golive/GoLiveScreen.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/golive/GoLiveScreen.kt
@@ -1,20 +1,35 @@
 package com.soho.sohoapp.live.ui.view.screens.golive
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.test.bottom
+import androidx.compose.ui.test.top
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.NavController
+import com.soho.sohoapp.live.ui.components.ButtonColoured
 import com.soho.sohoapp.live.ui.components.SearchBar
+import com.soho.sohoapp.live.ui.components.SpacerVertical
 import com.soho.sohoapp.live.ui.components.Text400_14sp
 import com.soho.sohoapp.live.ui.components.Text700_14sp
 import com.soho.sohoapp.live.ui.components.Text950_20sp
 import com.soho.sohoapp.live.ui.components.brushMainGradientBg
+import com.soho.sohoapp.live.ui.components.brushMainGradientTransBg
+import com.soho.sohoapp.live.ui.theme.AppGreen
 import com.soho.sohoapp.live.utility.NetworkUtils
 import org.koin.compose.koinInject
 
@@ -30,20 +45,87 @@ fun GoLiveScreen(
             .background(brushMainGradientBg)
     ) {
         ScreenContent()
+        BottomNextButton()
+    }
+}
+
+/*@Composable
+fun HomeScreen() {
+    val scrollState = rememberScrollState()
+
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+        val (topBar, content, submitButton) = FocusRequester.createRefs()
+
+        // Top action bar
+        TopAppBar(
+            title = { Text("Home") },
+            modifier = Modifier
+                .constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+
+        // Middle content list
+        LazyColumn(
+            modifier = Modifier
+                .constrainAs(content) {
+                    top.linkTo(topBar.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(submitButton.top)
+                }
+        ) {
+            // Add your content list items here
+        }
+
+        // Submit button
+        Button(
+            onClick = { *//* Handle submit button click *//* },
+            modifier = Modifier
+                .constrainAs(submitButton) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            Text("Submit")
+        }
+    }
+}*/
+
+
+@Composable
+fun BottomNextButton() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(brushMainGradientTransBg)
+            .padding(16.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        ButtonColoured(text = "Next", color = AppGreen) {
+
+        }
     }
 }
 
 @Composable
-fun ScreenContent() {
+private fun ScreenContent() {
     Column(
         modifier = Modifier.padding(
             horizontal = 16.dp, vertical = 24.dp
         )
     ) {
         StepCountTitleInfo()
-        Spacer(modifier = Modifier.height(40.dp))
+        SpacerVertical(40.dp)
         SearchBar()
-        Spacer(modifier = Modifier.height(16.dp))
+        SpacerVertical(16.dp)
     }
 }
 
@@ -51,8 +133,8 @@ fun ScreenContent() {
 @Composable
 fun StepCountTitleInfo() {
     Text700_14sp(step = "Step 1 of 4")
-    Spacer(modifier = Modifier.height(8.dp))
+    SpacerVertical(8.dp)
     Text950_20sp(title = "Link livestream to your property")
-    Spacer(modifier = Modifier.height(8.dp))
+    SpacerVertical(8.dp)
     Text400_14sp(info = "Prospect interested in your listing will be notified of your scheduled livestream and will be livecasted on your property listing page.")
 }
