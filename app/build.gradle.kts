6aa9a9bfd5d6badcc863aa36107b2020db4891ed
Amal Ranasinghe <amal@soho.com.au>
2024-06-26 23:05:20 +0530

    371 - format libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlinSerialization)
-    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
