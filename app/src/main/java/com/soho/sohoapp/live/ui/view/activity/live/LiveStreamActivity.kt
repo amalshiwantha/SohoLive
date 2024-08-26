d56aa439e623cf07f686f8db86130cc852cb754a
Amal Ranasinghe <amal@soho.com.au>
2024-08-26 13:37:02 +0530

    393 - set version check for enableEdgeToEdgeFullScreen

--- app/src/main/java/com/soho/sohoapp/live/ui/view/activity/live/LiveStreamActivity.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/activity/live/LiveStreamActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.SurfaceHolder
@@ -103,14 +104,26 @@ class LiveStreamActivity : AppCompatActivity() {
 
     // Function to enable edge-to-edge mode and set full screen
     private fun enableEdgeToEdgeFullScreen() {
-        window.decorView.post {
-            window.decorView.windowInsetsController?.let { controller ->
-                // Hide system bars
-                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                // Make sure that the system bars remain hidden
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.decorView.post {
+                window.decorView.windowInsetsController?.let { controller ->
+                    // Hide system bars
+                    controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                    // Make sure that the system bars remain hidden
+                    controller.systemBarsBehavior =
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+                }
             }
+        } else {
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
         }
     }
 
