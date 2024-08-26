cc0df4dbb0f14ed109e68d7c5166b932dc7c8e03
Amal Ranasinghe <amal@soho.com.au>
2024-08-26 12:08:49 +0530

    393 - SelectOrientationBottomSheet add title and buttons

--- app/src/main/java/com/soho/sohoapp/live/ui/components/BottomSheet.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/components/BottomSheet.kt
@@ -4,21 +4,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.BottomSheetDefaults.DragHandle
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -29,10 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.soho.sohoapp.live.R
 import com.soho.sohoapp.live.enums.Orientation
 import com.soho.sohoapp.live.model.MainStateHolder
 import com.soho.sohoapp.live.ui.theme.BottomBarBg
@@ -57,20 +47,13 @@ fun SelectOrientationBottomSheet(onGoLive: () -> Unit, onCancel: () -> Unit) {
         Column(
             modifier = Modifier
                 .padding(16.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
         ) {
-            Text(
-                text = "Select Livecast Orientation",
-                style = MaterialTheme.typography.titleLarge.copy(fontSize = 18.sp),
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Text(
-                text = "This will determine the orientation for this livecast.",
-                style = MaterialTheme.typography.bodySmall,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = 24.dp)
-            )
+            Text800_20sp(label = "Select Livecast Orientation")
+            SpacerUp(size = 8.dp)
+
+            Text400_14sp(info = "This will determine the orientation for this livecast.")
+            SpacerUp(size = 24.dp)
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -88,41 +71,33 @@ fun SelectOrientationBottomSheet(onGoLive: () -> Unit, onCancel: () -> Unit) {
                 )
             }
 
-            Spacer(modifier = Modifier.height(24.dp))
+            SpacerUp(size = 24.dp)
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                OutlinedButton(
-                    onClick = {
+                ButtonOutlineWhite(
+                    text = "Cancel",
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(48.dp),
+                    onBtnClick = {
                         scope.launch {
                             bottomSheetState.hide()
                             onCancel()
                         }
-                    },
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Text("Cancel")
-                }
+                    })
 
-                Spacer(modifier = Modifier.width(8.dp))
+                SpacerSide(size = 8.dp)
 
-                Button(
-                    onClick = {
+                ButtonGradientIcon(text = "Preview Live",
+                    icon = R.drawable.livecast_color,
+                    gradientBrush = brushGradientLive,
+                    onBtnClick = {
                         onGoLive()
-                    },
-                    colors = ButtonDefaults.buttonColors(contentColor = Color.Red),
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Icon(
-                        Icons.Default.PlayArrow,
-                        contentDescription = "Preview Live",
-                        tint = Color.White
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text("Preview Live", color = Color.White)
-                }
+                    })
+
             }
         }
     }
