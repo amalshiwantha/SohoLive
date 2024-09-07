6cc11f11117f913f09e661d09db6865d826b9fa4
Amal Ranasinghe <amal@soho.com.au>
2024-09-07 22:43:33 +0530

    397 - set scrollable content for landscape view

--- app/src/main/java/com/soho/sohoapp/live/ui/components/Alerts.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/components/Alerts.kt
@@ -1,14 +1,17 @@
 package com.soho.sohoapp.live.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -27,12 +30,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.soho.sohoapp.live.R
 import com.soho.sohoapp.live.enums.AlertConfig
 import com.soho.sohoapp.live.enums.SocialMediaInfo
 import com.soho.sohoapp.live.model.SmBtn
 import com.soho.sohoapp.live.ui.theme.AppGreen
 import com.soho.sohoapp.live.ui.theme.AppGreenDark
+import com.soho.sohoapp.live.ui.theme.AppWhite
 import com.soho.sohoapp.live.ui.theme.BottomBarUnselect
 import com.soho.sohoapp.live.ui.theme.FacebookBlue
 import com.soho.sohoapp.live.ui.theme.FacebookBlueDark
@@ -99,7 +105,168 @@ fun ShareableLinkDialog(
             color = Color.White,
             modifier = Modifier
         ) {
-            Column {
+
+            ConstraintLayout(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(AppWhite)
+            ) {
+                val (content, goLiveBtn) = createRefs()
+
+                LazyColumn(
+                    modifier = Modifier
+                        .constrainAs(content) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(goLiveBtn.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
+                ) {
+                    item {
+                        //Title and Close
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_share),
+                                contentDescription = null,
+                            )
+                            SpacerSide(size = 8.dp)
+                            Text950_20sp(
+                                title = "Shareable Links",
+                                txtColor = infoText,
+                                modifier = Modifier.weight(1f)
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_round_cross),
+                                contentDescription = null,
+                                modifier = Modifier.clickable { onDismiss() }
+                            )
+                        }
+
+                        //content
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+
+                            // Description
+                            SpacerUp(size = 8.dp)
+                            Text400_14sp(
+                                info = "Copy and share your links before you go live. This will reduce disruptions once you are live.",
+                                color = infoText
+                            )
+
+                            //SohoLogo
+                            SpacerUp(size = 24.dp)
+                            Image(
+                                painter = painterResource(id = R.drawable.logo_soho),
+                                contentDescription = null
+                            )
+                            SpacerUp(size = 8.dp)
+                            Text400_14sp(
+                                info = "Videos will be shown on the property listing.",
+                                color = infoText
+                            )
+
+                            //Copy Btn
+                            SpacerUp(size = 16.dp)
+                            ButtonColoredIcon(
+                                title = smBtnSoho.txt,
+                                btnColor = smBtnSoho.color,
+                                icon = smBtnSoho.icon,
+                                onBtnClick = {
+                                    onClickCopy(SocialMediaInfo.SOHO.name)
+                                    isCopiedSoho = true
+                                })
+                            SpacerUp(size = 16.dp)
+                        }
+
+                        if (isShowLiveBtn) {
+                            Divider(color = BottomBarUnselect)
+                            SpacerUp(size = 16.dp)
+                        }
+
+                        // Social Link
+                        if (isHasSMLinks) {
+                            Column(
+                                modifier = Modifier.padding(horizontal = 16.dp)
+                            ) {
+                                Text700_14sp(
+                                    step = "Social Links",
+                                    color = infoText
+                                )
+                                SpacerUp(size = 8.dp)
+                                Text400_14sp(
+                                    info = "You can also share watching links for your connected socials",
+                                    color = infoText
+                                )
+
+                                // Social Media Buttons
+                                SpacerUp(size = 24.dp)
+                                Column {
+                                    SocialLinkButton(
+                                        smItem = SocialMediaInfo.FACEBOOK,
+                                        onClick = {
+                                            onClickCopy(SocialMediaInfo.FACEBOOK.name)
+                                            isCopiedFacebook = true
+                                        },
+                                        smBtn = smBtnFb
+                                    )
+                                    SpacerUp(size = 16.dp)
+                                    SocialLinkButton(
+                                        smItem = SocialMediaInfo.YOUTUBE,
+                                        onClick = {
+                                            onClickCopy(SocialMediaInfo.YOUTUBE.name)
+                                            isCopiedYoutube = true
+                                        },
+                                        smBtn = smBtnYt
+                                    )
+                                    SpacerUp(size = 16.dp)
+                                    SocialLinkButton(
+                                        smItem = SocialMediaInfo.LINKEDIN,
+                                        onClick = {
+                                            onClickCopy(SocialMediaInfo.LINKEDIN.name)
+                                            isCopiedLinkedIn = true
+                                        },
+                                        smBtn = smBtnLi
+                                    )
+                                }
+                            }
+
+                            SpacerUp(size = 24.dp)
+                            Divider(color = BottomBarUnselect)
+                        }
+                    }
+                }
+
+                // Go Live Now Button
+                if (isShowLiveBtn) {
+                    SpacerUp(size = 8.dp)
+                    Column(
+                        modifier = Modifier
+                            .constrainAs(goLiveBtn) {
+                                bottom.linkTo(parent.bottom)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }
+                            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                    ) {
+                        ButtonGradientIcon(
+                            text = "Go Live Now",
+                            modifier = Modifier.fillMaxWidth(),
+                            gradientBrush = brushGradientLive,
+                            icon = R.drawable.livecast_color,
+                            onBtnClick = {
+                                onClickLive()
+                            })
+                    }
+                }
+            }
+
+            /*Column {
 
                 // Title and close btn
                 Column(
@@ -229,7 +396,7 @@ fun ShareableLinkDialog(
                             })
                     }
                 }
-            }
+            }*/
         }
     }
 }
