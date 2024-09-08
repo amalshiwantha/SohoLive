1bfc06492b275282c96774963fb1fe4b4cc77039
Amal Ranasinghe <amal@soho.com.au>
2024-09-08 21:55:29 +0530

    397 - onBoard : designed main layout for onBoard view

--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/pre_access/PreAccessScreen.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/pre_access/PreAccessScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,10 +24,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavHostController
 import com.soho.sohoapp.live.R
 import com.soho.sohoapp.live.ui.components.ButtonColoured
 import com.soho.sohoapp.live.ui.components.ButtonOutlineWhite
+import com.soho.sohoapp.live.ui.components.Text800_12sp
 import com.soho.sohoapp.live.ui.components.brushMainGradientBg
 import com.soho.sohoapp.live.ui.navigation.NavigationPath
 import com.soho.sohoapp.live.ui.theme.AppGreen
@@ -34,6 +38,61 @@ import com.soho.sohoapp.live.ui.theme.AppWhite
 
 @Composable
 fun PreAccessScreen(modifier: Modifier = Modifier, navController: NavHostController) {
+
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brushMainGradientBg)
+    ) {
+        val (topLogo, onBoard, button) = createRefs()
+
+        // Top static logo
+        Image(
+            painter = painterResource(id = R.drawable.soho_livecast_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(top = 40.dp)
+                .constrainAs(topLogo) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+
+        // Middle scrollable content
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(onBoard) {
+                    top.linkTo(topLogo.bottom)
+                    bottom.linkTo(button.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    height = Dimension.fillToConstraints
+                }
+        ) {
+            items(10) {
+                Text800_12sp(label = "Nikan")
+            }
+        }
+
+        // Bottom static buttons
+        BottomButtons(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(button) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            navController = navController
+        )
+    }
+
+}
+
+/*private fun oldView(){
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -43,7 +102,7 @@ fun PreAccessScreen(modifier: Modifier = Modifier, navController: NavHostControl
         CenterImgText(modifier)
         BottomButtons(modifier.align(alignment = Alignment.BottomCenter), navController)
     }
-}
+}*/
 
 @Composable
 fun BottomButtons(modifier: Modifier, navController: NavHostController) {
@@ -58,10 +117,10 @@ fun BottomButtons(modifier: Modifier, navController: NavHostController) {
             color = AppGreen,
             onBtnClick = { navController.navigate(NavigationPath.SIGNIN.name) })
 
-        ButtonOutlineWhite(
+        /*ButtonOutlineWhite(
             text = stringResource(R.string.sign_up),
             modifier = Modifier.fillMaxWidth(),
-            onBtnClick = { navController.navigate(NavigationPath.SIGNUP.name) })
+            onBtnClick = { navController.navigate(NavigationPath.SIGNUP.name) })*/
     }
 }
 
