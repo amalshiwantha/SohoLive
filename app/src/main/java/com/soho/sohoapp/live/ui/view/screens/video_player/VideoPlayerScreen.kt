7eb66486123bb699fa5fd62495e0dd8519ef2807
Amal Ranasinghe <amal@soho.com.au>
2024-09-16 09:06:29 +0530

    399 - added back button to the video player screen

--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_player/VideoPlayerScreen.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_player/VideoPlayerScreen.kt
@@ -1,12 +1,13 @@
 package com.soho.sohoapp.live.ui.view.screens.video_player
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
@@ -14,16 +15,24 @@ import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.navigation.NavHostController
+import com.soho.sohoapp.live.R
+import com.soho.sohoapp.live.ui.components.AppTopBar
+import com.soho.sohoapp.live.ui.components.AppTopBarCustom
+import com.soho.sohoapp.live.ui.components.TopAppBarCustomClose
 import com.soho.sohoapp.live.ui.components.brushMainGradientBg
 
 @Composable
 fun VideoPlayerScreen(navController: NavHostController, title: String, url: String) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(brushMainGradientBg),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(brushMainGradientBg)
     ) {
+        AppTopBarCustom(title = title,
+            modifier = Modifier.fillMaxWidth(),
+            onBackClick = { navController.popBackStack() })
+
         ExoPlayerView(
             url = url,
             modifier = Modifier.fillMaxSize()
