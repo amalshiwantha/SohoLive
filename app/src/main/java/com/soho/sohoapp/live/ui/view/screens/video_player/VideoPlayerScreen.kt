0b280f64483f931a9802ad74ef46a5e5419ebad4
Amal Ranasinghe <amal@soho.com.au>
2024-09-16 12:27:55 +0530

    399 - added progress bar for video loading

--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_player/VideoPlayerScreen.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_player/VideoPlayerScreen.kt
@@ -1,25 +1,28 @@
 package com.soho.sohoapp.live.ui.view.screens.video_player
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.navigation.NavHostController
-import com.soho.sohoapp.live.R
-import com.soho.sohoapp.live.ui.components.AppTopBar
 import com.soho.sohoapp.live.ui.components.AppTopBarCustom
-import com.soho.sohoapp.live.ui.components.TopAppBarCustomClose
+import com.soho.sohoapp.live.ui.components.CenterMessageProgress
 import com.soho.sohoapp.live.ui.components.brushMainGradientBg
+import com.soho.sohoapp.live.ui.theme.AppGreen
 
 @Composable
 fun VideoPlayerScreen(navController: NavHostController, title: String, url: String) {
@@ -44,6 +47,7 @@ fun VideoPlayerScreen(navController: NavHostController, title: String, url: Stri
 @Composable
 private fun ExoPlayerView(url: String, modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    var isStartPlay by remember { mutableStateOf(false) }
 
     // Initialize ExoPlayer
     val exoPlayer = remember {
@@ -55,15 +59,40 @@ private fun ExoPlayerView(url: String, modifier: Modifier = Modifier) {
         }
     }
 
-    DisposableEffect(
-        AndroidView(
-            modifier = modifier,
-            factory = {
-                PlayerView(context).apply {
-                    player = exoPlayer
+    // Set up a listener for buffering state
+    DisposableEffect(Unit) {
+        val listener = object : Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+
+                if (isPlaying) {
+                    isStartPlay = true
                 }
+
             }
-        )
+        }
+        exoPlayer.addListener(listener)
+
+        onDispose {
+            exoPlayer.removeListener(listener)
+            exoPlayer.release()
+        }
+    }
+
+    DisposableEffect(
+
+        if (isStartPlay) {
+            AndroidView (
+                modifier = modifier,
+                factory = {
+                    PlayerView(context).apply {
+                        player = exoPlayer
+                    }
+                }
+            )
+        } else {
+            CenterMessageProgress(message = "Video Loading...")
+        }
     ) {
         onDispose {
             exoPlayer.release()
