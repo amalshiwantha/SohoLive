0c80eb60b3778eb66c93ca4630874a0cda485b70
Amal Ranasinghe <amal@soho.com.au>
2024-09-26 12:11:04 +0530

    399 - Step #1 : search field icon and padding issue fixed

--- app/src/main/java/com/soho/sohoapp/live/ui/components/Search.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/components/Search.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.TextField
@@ -24,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -45,9 +44,8 @@ fun SearchBar() {
             .background(Color.White, shape = RoundedCornerShape(16.dp))
     ) {
         Icon(
-            imageVector = Icons.Default.Search,
+            painter = painterResource(id = R.drawable.ic_search),
             contentDescription = "Search Icon",
-            tint = Color.Black,
             modifier = Modifier.padding(start = 16.dp)
         )
         TextField(
