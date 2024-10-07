27c723e5aeac8c372358724a7100cd8d29c5c143
Amal Ranasinghe <amal@soho.com.au>
2024-10-07 21:49:44 +0530

    399 - internal test release v11(1.0.1.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.soho.sohoapp.live"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.1.2"
+        versionCode = 11
+        versionName = "1.0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
