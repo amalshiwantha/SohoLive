2666e6d0b6cb8dadf1ede1003fdcd934f4d231c4
Amal Ranasinghe <amal@soho.com.au>
2024-10-16 23:07:57 +0530

    402 - add row desing for the preRec vid item

--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_recorder/PreRecordVideoScreen.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_recorder/PreRecordVideoScreen.kt
@@ -11,10 +11,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,7 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
@@ -35,7 +36,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.soho.sohoapp.live.R
 import com.soho.sohoapp.live.ui.components.AppTopBar
-import com.soho.sohoapp.live.ui.components.SpacerUp
+import com.soho.sohoapp.live.ui.components.SpacerSide
 import com.soho.sohoapp.live.ui.components.Text700_14sp
 import com.soho.sohoapp.live.ui.components.Text700_14spBold
 import com.soho.sohoapp.live.ui.components.brushMainGradientBg
@@ -96,40 +97,40 @@ fun MainContent(onPlay: (Uri) -> Unit) {
 fun VideoFileItem(file: File, onPlay: (Uri) -> Unit) {
     val thumbnail = remember { getVideoThumbnail(file) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .clickable {
-                onPlay(Uri.fromFile(file))
-            }
-    ) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+        .clickable {
+            onPlay(Uri.fromFile(file))
+        }) {
+
+        //thumbnail
+        val commonMod = Modifier
+            .size(100.dp)
+            .background(Color.Gray, RoundedCornerShape(8.dp))
+
         if (thumbnail != null) {
             Image(
                 bitmap = thumbnail.asImageBitmap(),
                 contentDescription = "Video thumbnail",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .clip(RoundedCornerShape(8.dp)),
+                modifier = commonMod,
                 contentScale = ContentScale.Crop
             )
         } else {
             Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .background(Color.Gray, RoundedCornerShape(8.dp)),
+                modifier = commonMod,
                 contentAlignment = Alignment.Center
             ) {
                 Text("No Thumbnail Available", color = Color.White)
             }
         }
 
-        SpacerUp(size = 8.dp)
+        SpacerSide(size = 8.dp)
 
-        Text700_14sp(step = file.name)
-        Text700_14spBold(step = "Size: ${file.length() / (1024 * 1024)} MB")
+        Column(modifier = Modifier.weight(1f)) {
+            Text700_14sp(step = file.name)
+            Text700_14spBold(step = "Size: ${file.length() / (1024 * 1024)} MB")
+        }
     }
 }
 
