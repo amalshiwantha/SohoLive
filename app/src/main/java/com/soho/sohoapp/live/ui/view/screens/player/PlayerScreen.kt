0fb48d31aab3f000002166bad30eae1512ee1ce8
Amal Ranasinghe <amal@soho.com.au>
2024-10-26 01:46:49 +0530

    402 - Pre_Rec Player : added play button to the center

--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/player/PlayerScreen.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/player/PlayerScreen.kt
@@ -3,6 +3,7 @@ package com.soho.sohoapp.live.ui.view.screens.player
 import android.net.Uri
 import android.widget.MediaController
 import android.widget.VideoView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,6 +12,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
@@ -102,6 +106,7 @@ fun PlayerScreen(navController: NavHostController, fileUri: Uri) {
             ) {
 
                 Box(modifier = Modifier.fillMaxSize()) {
+                    //Player
                     AndroidView(
                         factory = { context ->
                             // Create a VideoView
@@ -126,6 +131,21 @@ fun PlayerScreen(navController: NavHostController, fileUri: Uri) {
                         },
                         modifier = Modifier.fillMaxSize()
                     )
+
+                    // Play IconButton in the center
+                    IconButton(
+                        onClick = {
+                            // Logic to start/pause the video
+                        },
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(56.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_play),
+                            contentDescription = "Play"
+                        )
+                    }
                 }
 
             }
