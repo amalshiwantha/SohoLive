1261a7b60313d6f18c0459747736c3ea07e046d2
Amal Ranasinghe <amal@soho.com.au>
2024-12-13 07:54:35 +0530

    407 - feedback : updated title and subtitle in all of steps

--- app/src/main/java/com/soho/sohoapp/live/enums/StepInfo.kt
+++ app/src/main/java/com/soho/sohoapp/live/enums/StepInfo.kt
@@ -10,22 +10,22 @@ enum class StepInfo(
 ) {
     STEP_1(
         counter = "Step 1 of $maxSteps",
-        title = "Link livestream to your property",
-        info = "Prospect interested in your listing will be notified of your scheduled livestream and will be livecasted on your property listing page."
+        title = "Link video to your property",
+        info = "Prospects interested in your property listing will be notified when video is available, and it will be featured directly on your property listing page. "
     ),
     STEP_2(
         counter = "Step 2 of $maxSteps",
-        title = "Select profile to go live",
-        info = "Which profile do you want to showcase when you go live?"
+        title = "Select profile to go promote",
+        info = "The selected profile’s photo, name, and star rating (if available) will be displayed on the video."
     ),
     STEP_3(
         counter = "Step 3 of $maxSteps",
-        title = "Select your video format",
-        info = "You can either stream live, allowing viewers to watch in real-time on your property listing, or record your video and share it at a later time."
+        title = "Give your audience more information on your video",
+        info = "Provide details that will allow us to better inform prospective buyers and renters what your video is about."
     ),
     STEP_4(
         counter = "Step 4 of $maxSteps",
-        title = "Give your audience more information on your livestream",
-        info = "Providing these details will allow us to better inform prospective buyers and renters what your livestream is about."
+        title = "Select your video format",
+        info = "You can either stream live, allowing viewers to watch in real-time on your property listing, or record your video and share it at a later time."
     )
 }
