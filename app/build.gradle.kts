6d7b12e5d4e42d0430a9224d21dcb4aa7591a36b
Amal Ranasinghe <amal@soho.com.au>
2025-02-25 08:39:05 +0530

    416 - dev release v26 (1.0.4.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.soho.sohoapp.live"
         minSdk = 24
         targetSdk = 34
-        versionCode = 25
-        versionName = "1.0.4.1"
+        versionCode = 26
+        versionName = "1.0.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
