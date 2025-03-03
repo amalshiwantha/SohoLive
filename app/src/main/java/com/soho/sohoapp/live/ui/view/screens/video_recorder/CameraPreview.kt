afc27a7ca0bcdf82e7fecefe6e8080f0c295095b
Amal Ranasinghe <amal@soho.com.au>
2025-03-03 13:55:34 +0530

    418 - set aspectRatio for video record

--- app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_recorder/CameraPreview.kt
+++ app/src/main/java/com/soho/sohoapp/live/ui/view/screens/video_recorder/CameraPreview.kt
@@ -4,6 +4,10 @@ import androidx.camera.video.Quality
 import androidx.camera.video.QualitySelector
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -18,6 +22,7 @@ fun CameraPreview(
     isRecording: Boolean = false
 ) {
     val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
+    val aspectRatio = if (isLandscape) 9f / 16f else 16f / 9f
 
     LaunchedEffect(controller) {
         if (isRecording) {
@@ -25,21 +30,58 @@ fun CameraPreview(
         }
     }
 
-    AndroidView(
-        factory = {
-            PreviewView(it).apply {
-                this.controller = controller
-                controller.bindToLifecycle(lifecycleOwner)
+    if (isRecording) {
+        Box(
+            modifier = modifier
+                .aspectRatio(aspectRatio)
+                .fillMaxWidth()
+        ) {
+            AndroidView(
+                factory = {
+                    PreviewView(it).apply {
+                        this.controller = controller
+                        controller.bindToLifecycle(lifecycleOwner)
+                        this.scaleType = PreviewView.ScaleType.FIT_CENTER
 
-                if (camPadding.first != 0 && camPadding.second != 0) {
-                    if (isLandscape) {
-                        this.setPadding(camPadding.second, camPadding.second, 0, camPadding.second)
-                    } else {
-                        this.setPadding(camPadding.first, 0, camPadding.first, 0)
+                        if (camPadding.first != 0 && camPadding.second != 0) {
+                            if (isLandscape) {
+                                this.setPadding(
+                                    camPadding.second,
+                                    camPadding.second,
+                                    0,
+                                    camPadding.second
+                                )
+                            } else {
+                                this.setPadding(camPadding.first, 0, camPadding.first, 0)
+                            }
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    } else {
+        AndroidView(
+            factory = {
+                PreviewView(it).apply {
+                    this.controller = controller
+                    controller.bindToLifecycle(lifecycleOwner)
+
+                    if (camPadding.first != 0 && camPadding.second != 0) {
+                        if (isLandscape) {
+                            this.setPadding(
+                                camPadding.second,
+                                camPadding.second,
+                                0,
+                                camPadding.second
+                            )
+                        } else {
+                            this.setPadding(camPadding.first, 0, camPadding.first, 0)
+                        }
                     }
                 }
-            }
-        },
-        modifier = modifier
-    )
+            },
+            modifier = modifier
+        )
+    }
 }
