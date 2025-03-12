ab6c486bb8949dfe0be6994871953615a72049e4
Amal Ranasinghe <amal@soho.com.au>
2025-03-12 12:52:05 +0530

    417 - Live Release v31 (1.0.6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 31
-        versionName = "1.0.5.1-Alpha"
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
